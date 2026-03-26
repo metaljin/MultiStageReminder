c712c7b354270d67fcfbe4d0a4894d93a8f8dbf1
metaljin <shinan920@163.com>
2026-03-26 16:30:46 +0800

    Create EditTemplateActivity.kt

---
+++ app/src/main/java/com/reminder/multistage/EditTemplateActivity.kt
@@ -0,0 +1,10 @@
+package com.reminder.multistage
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditTemplateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
