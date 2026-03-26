ac7c3a0c59c08de4badffd40e2b2791872f3b279
metaljin <shinan920@163.com>
2026-03-26 16:26:49 +0800

    Create Template.kt

---
+++ app/src/main/java/com/reminder/multistage/Template.kt
@@ -0,0 +1,10 @@
+package com.reminder.multistage
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "templates")
+data class Template(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String,
+    val totalCycles: Int
+)
