78818a2a4230b17993893443b51416707f816340
metaljin <shinan920@163.com>
2026-03-27 14:12:41 +0800

    Update EditTemplateActivity.kt

--- app/src/main/java/com/reminder/multistage/EditTemplateActivity.kt
+++ app/src/main/java/com/reminder/multistage/EditTemplateActivity.kt
@@ -16,7 +16,7 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.MusicNote
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
